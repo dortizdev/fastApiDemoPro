6640331c55166f64011dd14762896853fdd83a76
Daniel Ortiz <danielortiz08@gmail.com>
2024-01-11 20:34:42 -0500

    Debug issues related to asyncio concurrency and add redis connection

--- inventory/main.py
+++ inventory/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from redis_om import get_redis_connection
 
 app = FastAPI()
 
+redis = get_redis_connection(
+    host=env.host,
+    port=env.port,
+    password=env.password,
+    decode_responses=True
+)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class Product():
+    name: str
+    price: float
+    quantity: int
+
+    class Meta:
+        database = redis
+
+@app.get('/products')
+async def all():
+    return []
