64521a713554998b23bd80503097004c5dc0fc87
Daniel Ortiz <danielortiz08@gmail.com>
2024-01-12 23:18:33 -0500

    Update main file to wwork with dotenv package

--- inventory/main.py
+++ inventory/main.py
@@ -1,16 +1,28 @@
+import os
 from fastapi import FastAPI
-from redis_om import get_redis_connection
+from fastapi.middleware.cors import CORSMiddleware
+from redis_om import get_redis_connection, HashModel
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000'],
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
 redis = get_redis_connection(
-    host=env.host,
-    port=env.port,
-    password=env.password,
+    host=os.getenv("host"),
+    port=os.getenv("port"),
+    password=os.getenv("password"),
     decode_responses=True
 )
 
-class Product():
+class Product(HashModel):
     name: str
     price: float
     quantity: int
@@ -19,5 +31,19 @@ class Product():
         database = redis
 
 @app.get('/products')
-async def all():
-    return []
+def all():
+    return [format(pk) for pk in Product.all_pks()]
+
+def format(pk: str):
+    product = Product.get(pk)
+
+    return {
+        'id': product.pk,
+        'name': product.name,
+        'price': product.price,
+        'quantity': product.quantity
+    }
+
+@app.post('/products')
+def create(product: Product):
+    return product.save()
